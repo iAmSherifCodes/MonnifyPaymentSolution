07518ecc2a499035c504ae8f5c930b8de29d3fff
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-08 22:38:06 +0100

    Added Constants fields

---
+++ src/main/java/com/wallet/monnify/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.wallet.monnify.utils;
+
+
+public class Constants {
+    public final static String DIGITS="0123456789";
+    public final static String ALPHABETS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    public final static String EMAIL_EXISTS="Email already exists";
+    public final static String USER_EMAIL_NOT_EXISTS="User email not found";
+    public final static String WRONG_PASSWORD="Wrong password";
+}
