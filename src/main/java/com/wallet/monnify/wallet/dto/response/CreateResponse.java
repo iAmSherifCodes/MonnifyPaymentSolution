69bb474faa28c769fbeba799f1e8fd9a15cb0919
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-08 18:53:14 +0100

    CreateResponse dto

--- src/main/java/com/wallet/monnify/wallet/dto/response/CreateResponse.java
+++ src/main/java/com/wallet/monnify/wallet/dto/response/CreateResponse.java
@@ -1,9 +1,12 @@
 package com.wallet.monnify.wallet.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class CreateResponse {
-    private String message;
+    @JsonProperty(value = "responseBody")
+    private ReservedAccountResponse responseBody;
 }
