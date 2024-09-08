85d271d620f9ce5fb54f8370056d766e3e3484eb
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-08 18:48:59 +0100

    Created custom repository method in AccountRepository

--- src/main/java/com/wallet/monnify/wallet/data/repository/AccountRepository.java
+++ src/main/java/com/wallet/monnify/wallet/data/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import com.wallet.monnify.wallet.data.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends MongoRepository<Account, String> {
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
