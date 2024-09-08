d6a6375c1e0a91ed88963fb2973f7afdb63f7ac7
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-08 18:54:52 +0100

    Account interface service

--- src/main/java/com/wallet/monnify/wallet/services/IAccount.java
+++ src/main/java/com/wallet/monnify/wallet/services/IAccount.java
@@ -1,10 +1,13 @@
 package com.wallet.monnify.wallet.services;
 
 import com.wallet.monnify.wallet.dto.request.CreateRequest;
-import com.wallet.monnify.wallet.dto.response.CreateResponse;
+import com.wallet.monnify.wallet.dto.request.GetAccountBalanceRequest;
+import com.wallet.monnify.wallet.dto.request.GetReservedAccountRequest;
+import com.wallet.monnify.wallet.dto.response.BalanceResponse;
 import com.wallet.monnify.wallet.dto.response.ReservedAccountResponse;
 
 public interface IAccount {
-    CreateResponse createReservedAccount(CreateRequest createRequest);
-    ReservedAccountResponse getReservedAccount (String accountReference);
+    ReservedAccountResponse createReservedAccount(CreateRequest createRequest) throws Exception;
+    ReservedAccountResponse getReservedAccount (String accountReference, String apiToken) throws Exception;
+    BalanceResponse getAccountBalance(String accountNumber, String apiToken) throws Exception;
 }
