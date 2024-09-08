c1223bc9ad905343d8b74c090e44f664a262ba50
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-08 18:47:15 +0100

    Modified account model

--- src/main/java/com/wallet/monnify/wallet/data/model/Account.java
+++ src/main/java/com/wallet/monnify/wallet/data/model/Account.java
@@ -1,21 +1,39 @@
 package com.wallet.monnify.wallet.data.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-@Getter @Setter @Document
+@Getter @Setter @Document @Builder
 public class Account {
     @Id
     private String id;
-    private String accountName;
+    private String accountName;//
     private String accountNumber;
+    private String bankName;
     private BigDecimal balance;
-    private String currency;
-    @Indexed(unique = true)
+    private String currencyCode;
+    @Indexed(unique = true) @JsonProperty(value = "accountReference")
     private String accountReference;
+    @JsonProperty(value = "contractCode")
+    private String contractCode;
+    private String customerEmail;
+    private String customerName;
+    @JsonProperty(value = "accounts")
+    private List<BankAccount> accounts;
+    private String collectionChannel;
+    private String reservationReference;
+    private String reservedAccountType;//
+    private String status;
+    private String createdOn;//
+    private String bvn;
+    private String nin;
 }
