145d28eece7f179ea4fee14699230e6a68f7ddbc
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-10 18:57:35 +0100

    Added AppUtils file

---
+++ src/main/java/com/wallet/monnify/utils/AppUtils.java
@@ -0,0 +1,9 @@
+package com.wallet.monnify.utils;
+
+import java.util.List;
+
+public class AppUtils {
+    public static List<String> publicPath(){
+        return List.of("/api/v1", "/sign-in");
+    }
+}
