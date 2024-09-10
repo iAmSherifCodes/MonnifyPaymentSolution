9048d05a047a24e1fdd90f054fed9cd1ee7b20ee
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-10 23:19:20 +0100

    Stripped public endpoints to cors

--- src/main/java/com/wallet/monnify/security/MonnifySecurityConfig.java
+++ src/main/java/com/wallet/monnify/security/MonnifySecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,22 +16,30 @@ import static com.wallet.monnify.utils.Role.USER;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Configuration @AllArgsConstructor
+@Configuration @AllArgsConstructor @EnableWebSecurity(debug = true)
 public class MonnifySecurityConfig {
     private final AuthenticationManager authenticationManager;
 
     @Bean
     public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception {
-        final String[] publicEndPoints ={"/api/v1", "/sign-in"};
+//        final List<String> publicEndPoints = AppUtils.publicPath();
         return httpSecurity
                 .addFilterAt(new MonnifyAuthenticationFilter(authenticationManager),
                         UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new MonnifyAuthorizationFilter(), MonnifyAuthorizationFilter.class)
+                .addFilterBefore(new MonnifyAuthorizationFilter(), MonnifyAuthenticationFilter.class)
                 .sessionManagement(customizer->customizer.sessionCreationPolicy(STATELESS))
                 .csrf(c->c.disable())
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(c->c.requestMatchers(POST, publicEndPoints).permitAll())
+                .authorizeHttpRequests(c->c.requestMatchers(POST,  "/sign-in",
+                        "/v1/sign-up").permitAll())
                 .authorizeHttpRequests(c->c.anyRequest().hasAuthority(USER.name()))
                 .build();
+    }
+
+    private String[] publicEndpoints(){
+        return new String[]{
+                "/api/v1", "/sign-in",
+                "/v1/sign-up"
+        };
     }
 }
