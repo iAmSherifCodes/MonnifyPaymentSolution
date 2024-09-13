8f465e3df826a4ed9e6fe83ac2f4ad6848bb311a
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-13 03:13:14 +0100

    Added custom repository method in repo

--- src/main/java/com/wallet/monnify/user/data/repository/UserRepository.java
+++ src/main/java/com/wallet/monnify/user/data/repository/UserRepository.java
@@ -2,12 +2,16 @@ package com.wallet.monnify.user.data.repository;
 
 import com.wallet.monnify.user.data.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface
+UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findUserByEmail(String email);
+    @Query("{'account.accountReference':  ?0}")
+    Optional<User> findUserByAccount_AccountReference(String accountReference);
 }
