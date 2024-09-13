4b66de594ca23f44f546c4c1446cc9e50ae9bac6
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-13 03:11:40 +0100

    Configured caching

--- src/main/java/com/wallet/monnify/MonnifyApplication.java
+++ src/main/java/com/wallet/monnify/MonnifyApplication.java
@@ -2,8 +2,9 @@ package com.wallet.monnify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication @EnableCaching
 public class MonnifyApplication {
 
 	public static void main(String[] args) {
