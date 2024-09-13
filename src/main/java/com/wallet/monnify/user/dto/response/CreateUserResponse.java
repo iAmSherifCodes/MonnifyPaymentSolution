25da6a0ca097bbc460770ca57d5414e1a3c5f2a8
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-09-13 03:14:01 +0100

    Modifies CreateUserResponse class

--- src/main/java/com/wallet/monnify/user/dto/response/CreateUserResponse.java
+++ src/main/java/com/wallet/monnify/user/dto/response/CreateUserResponse.java
@@ -1,9 +1,10 @@
 package com.wallet.monnify.user.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @Builder
 public class CreateUserResponse {
     private String message;
 }
